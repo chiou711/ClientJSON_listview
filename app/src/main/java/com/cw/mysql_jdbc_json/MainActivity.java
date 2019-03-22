a885ff5e6671194c118bc9284defb27cd4dd2043
cw <chiou711@gmail.com>
2019-03-22 18:01:23 +0800

    add image to list view

--- app/src/main/java/com/cw/mysql_jdbc_json/MainActivity.java
+++ app/src/main/java/com/cw/mysql_jdbc_json/MainActivity.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (C) 2019 CW Chiu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cw.mysql_jdbc_json;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.content.Context;
 import android.content.Intent;
@@ -9,12 +25,15 @@ import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.cw.mysql_jdbc_json.uil.UilCommon;
 import com.google.android.youtube.player.YouTubeIntents;
 
 import org.apache.http.HttpResponse;
@@ -44,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
 		task.execute();
 
 		list = (ListView) findViewById(R.id.list_data);
+
+		UilCommon.init();
 	}
 
 	private class MyTask extends AsyncTask<Void,Void,Void>{
@@ -119,6 +140,10 @@ public class MainActivity extends AppCompatActivity {
 			aName = (TextView) view.findViewById(R.id.text_id);
 			aAddress = (TextView) view.findViewById(R.id.text_uri);
 			aTitle = (TextView) view.findViewById(R.id.text_title);
+			ImageView thumbPicture = (ImageView) view.findViewById(R.id.thumb_picture);
+			ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.thumb_progress);
+			String pictureUri = "http://img.youtube.com/vi/"+getYoutubeId(Uri[i])+"/0.jpg";
+			//System.out.println("pictureUri = " + pictureUri);
 
 			aName.setOnClickListener(new View.OnClickListener() {
 				@Override
@@ -132,6 +157,22 @@ public class MainActivity extends AppCompatActivity {
 			aName.setText(String.valueOf(Id[i]));
 			aAddress.setText(Uri[i]);
 			aTitle.setText(Title[i]);
+
+
+			// load bitmap to image view
+			try
+			{
+				new UtilImage_bitmapLoader(thumbPicture,
+						pictureUri,
+						progressBar,
+						UilCommon.optionsForFadeIn,
+						MainActivity.this);
+			}
+			catch(Exception e)
+			{
+				Log.e("PageAdapter_recycler", "UtilImage_bitmapLoader error");
+			}
+
 			return view;
 		}
 	}
---
+++ app/src/main/java/com/cw/mysql_jdbc_json/UtilImage_bitmapLoader.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2018 CW Chiu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cw.mysql_jdbc_json;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.cw.mysql_jdbc_json.uil.UilCommon;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+
+public class UtilImage_bitmapLoader
+{
+//	private Bitmap thumbnail;
+//	private AsyncTaskVideoBitmap mVideoAsyncTask;
+	private SimpleImageLoadingListener mSimpleUilListener, mSimpleUilListenerForVideo;
+	private ImageLoadingProgressListener mUilProgressListener;
+	private ProgressBar mProgressBar;
+	private ImageView mPicImageView;
+    Activity mAct;
+
+	public UtilImage_bitmapLoader(ImageView picImageView,
+	                              String mPictureUriInDB,
+	                              final ProgressBar progressBar,
+	                              DisplayImageOptions options,
+	                              Activity act )
+	{
+ 	    setLoadingListeners();
+	    mPicImageView = picImageView;
+	    mProgressBar = progressBar;
+	    mAct = act;
+		Uri imageUri = Uri.parse(mPictureUriInDB);
+		String pictureUri = imageUri.toString();
+		UilCommon.imageLoader
+				 .displayImage(	pictureUri,
+								mPicImageView,
+								options,
+								mSimpleUilListener,
+								mUilProgressListener);
+
+	}
+
+	private  void setLoadingListeners()
+    {
+        // set image loading listener
+        mSimpleUilListener = new SimpleImageLoadingListener() 
+        {
+      	    @Override
+      	    public void onLoadingStarted(String imageUri, View view)
+      	    {
+      		    mPicImageView.setVisibility(View.GONE);
+      		    mProgressBar.setProgress(0);
+      		    mProgressBar.setVisibility(View.VISIBLE);
+      	    }
+
+      	    @Override
+      	    public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+      	    {
+      		    mProgressBar.setVisibility(View.GONE);
+      		    mPicImageView.setVisibility(View.VISIBLE);
+
+				String message = null;
+				switch (failReason.getType()) {
+					case IO_ERROR:
+				        message = "Input/Output error";
+//						message = mAct.getResources().getString(R.string.file_not_found);
+						break;
+					case DECODING_ERROR:
+						message = "Image can't be decoded";
+						break;
+					case NETWORK_DENIED:
+						message = "Downloads are denied";
+						break;
+					case OUT_OF_MEMORY:
+						message = "Out Of Memory error";
+						break;
+					case UNKNOWN:
+						message = "Unknown error";
+						break;
+				}
+				Toast.makeText(mAct, message, Toast.LENGTH_SHORT).show();
+      	    }
+
+      	    @Override
+      	    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage)
+      	    {
+      		    super.onLoadingComplete(imageUri, view, loadedImage);
+      		    mProgressBar.setVisibility(View.GONE);
+      		    mPicImageView.setVisibility(View.VISIBLE);
+      	    }
+  		};
+
+  		// set image loading listener for video
+  		mSimpleUilListenerForVideo = new SimpleImageLoadingListener() 
+  		{
+  			@Override
+  			public void onLoadingStarted(String imageUri, View view)
+  			{
+  				mPicImageView.setVisibility(View.GONE);
+  				mProgressBar.setProgress(0);
+  				mProgressBar.setVisibility(View.VISIBLE);
+  			}
+
+  			@Override
+  			public void onLoadingFailed(String imageUri, View view, FailReason failReason)
+  			{
+  				mProgressBar.setVisibility(View.GONE);
+  				mPicImageView.setVisibility(View.VISIBLE);
+
+  			}
+
+  			@Override
+  			public void onLoadingComplete(final String imageUri, View view, Bitmap loadedImage)
+  			{
+  				super.onLoadingComplete(imageUri, view, loadedImage);
+  				mProgressBar.setVisibility(View.GONE);
+  				mPicImageView.setVisibility(View.VISIBLE);
+  			}
+  		};
+
+  		// Set image loading process listener
+  		mUilProgressListener = new ImageLoadingProgressListener() 
+  		{
+  			@Override
+  			public void onProgressUpdate(String imageUri, View view, int current, int total)
+  			{
+  				mProgressBar.setProgress(Math.round(100.0f * current / total));
+  			}
+  		};
+    }
+    
+}
