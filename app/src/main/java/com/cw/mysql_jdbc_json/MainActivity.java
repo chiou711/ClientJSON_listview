5e571fc6ed492d09c29207476deb0ce0e416b61d
cw <chiou711@gmail.com>
2019-03-22 16:16:02 +0800

    Initial commit

---
+++ app/src/main/java/com/cw/mysql_jdbc_json/MainActivity.java
@@ -0,0 +1,159 @@
+package com.cw.mysql_jdbc_json;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.android.youtube.player.YouTubeIntents;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+	String url = "http://10.1.1.3:8080/LiteNoteWeb/viewNote/viewNote3.jsp";
+	String strResult;
+	private ListView list;
+	int [] Id;
+	String [] Uri;
+	String [] Title;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		System.out.println("MainActivity / _onCreate");
+
+		MyTask task = new MyTask();
+		task.execute();
+
+		list = (ListView) findViewById(R.id.list_data);
+	}
+
+	private class MyTask extends AsyncTask<Void,Void,Void>{
+		private int i = 0;
+		@Override
+		protected Void doInBackground(Void... voids) {
+			System.out.println("MainActivity / MyTask /_doInBackground");
+			try {
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost(url);
+
+				HttpResponse response = client.execute(post);
+
+				strResult = EntityUtils.toString(response.getEntity(), HTTP.UTF_8);
+				System.out.println("strResult = " + strResult);
+
+				JSONArray jsonArray = new JSONArray(strResult);
+
+				//Set Array size
+				Id = new int[jsonArray.length()];
+				Uri = new String[jsonArray.length()];
+				Title = new String[jsonArray.length()];
+
+
+				for(int i=0;i<jsonArray.length();i++) {
+					JSONObject jsonObject = (JSONObject)jsonArray.get(i);
+					Id[i] = jsonObject.getInt("note_id");
+					Uri[i] = jsonObject.getString("note_link_uri");
+					Title[i] = jsonObject.getString("note_title");
+				}
+
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		@Override
+		protected void onPostExecute(Void Result){
+			MyAdapter adapter = new MyAdapter(MainActivity.this);
+			list.setAdapter(adapter);
+			super.onPostExecute(Result);
+		}
+	}
+
+	public class MyAdapter extends BaseAdapter {
+		private LayoutInflater inflater;
+		public MyAdapter(Context c){
+			inflater = LayoutInflater.from(c);
+		}
+		@Override
+		public int getCount() {
+			if(Id == null)
+				return 0;
+			else
+				return Id.length;
+		}
+
+		@Override
+		public Object getItem(int i) {
+			return Id[i];
+		}
+
+		@Override
+		public long getItemId(int i) {
+			return i;
+		}
+
+		@Override
+		public View getView(final int i, View view, ViewGroup viewGroup) {
+			view = inflater.inflate(R.layout.layout_adapter,null);
+			TextView aName,aAddress,aTitle;
+			aName = (TextView) view.findViewById(R.id.text_id);
+			aAddress = (TextView) view.findViewById(R.id.text_uri);
+			aTitle = (TextView) view.findViewById(R.id.text_title);
+
+			aName.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					String idStr = getYoutubeId(Uri[i]);
+					Intent intent = YouTubeIntents.createPlayVideoIntentWithOptions(MainActivity.this, idStr, false/*fullscreen*/, true/*finishOnEnd*/);
+					startActivity(intent);
+				}
+			});
+
+			aName.setText(String.valueOf(Id[i]));
+			aAddress.setText(Uri[i]);
+			aTitle.setText(Title[i]);
+			return view;
+		}
+	}
+
+
+	// Get YouTube Id
+	public static String getYoutubeId(String url) {
+
+		String videoId = "";
+
+		if (url != null && url.trim().length() > 0 && url.startsWith("http")) {
+			String expression = "^.*((youtu.be\\/)|(v\\/)|(\\/u\\/w\\/)|(embed\\/)|(watch\\?))\\??(v=)?([^#\\&\\?]*).*";
+			CharSequence input = url;
+			Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+			Matcher matcher = pattern.matcher(input);
+			if (matcher.matches()) {
+				String groupIndex1 = matcher.group(8);
+				if (groupIndex1 != null && groupIndex1.length() == 11)
+					videoId = groupIndex1;
+			}
+		}
+		return videoId;
+	}
+
+}
