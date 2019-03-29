bf19c494e833e62ad1f528bdc2567d6e3e0ce561
cw <chiou711@gmail.com>
2019-03-29 13:50:53 +0800

    add SSL and android TV support

--- app/src/main/java/com/cw/ClientJSON_listview/MainActivity.java
+++ app/src/main/java/com/cw/ClientJSON_listview/MainActivity.java
@@ -24,12 +24,20 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -46,9 +54,13 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
 public class MainActivity extends AppCompatActivity {
-	String url = "http://10.1.1.3:8080/LiteNoteWeb/viewNote/viewNote_json.jsp";
-	String strResult;
 	private ListView list;
 	int [] Id;
 	String [] Uri;
@@ -63,25 +75,70 @@ public class MainActivity extends AppCompatActivity {
 		MyTask task = new MyTask();
 		task.execute();
 
-		list = (ListView) findViewById(R.id.list_data);
+		list = findViewById(R.id.list_data);
 
 		UilCommon.init();
 	}
 
 	private class MyTask extends AsyncTask<Void,Void,Void>{
-		private int i = 0;
 		@Override
 		protected Void doInBackground(Void... voids) {
 			System.out.println("MainActivity / MyTask /_doInBackground");
+
+			String strResult = "";
+
+			// HTTPS POST
+			String project = "LiteNote";
+			String urlStr =  "https://" + project + ".ddns.net:8443/"+ project +"Web/viewNote/viewNote_json.jsp";
+			//refer https://stackoverflow.com/questions/16504527/how-to-do-an-https-post-from-android
 			try {
-				HttpClient client = new DefaultHttpClient();
-				HttpPost post = new HttpPost(url);
+				URL url = new URL(urlStr);
+				trustEveryone();
+				HttpsURLConnection urlConnection = ((HttpsURLConnection)url.openConnection());
 
-				HttpResponse response = client.execute(post);
+				// set Timeout and method
+				urlConnection.setReadTimeout(7000);
+				urlConnection.setConnectTimeout(7000);
+				urlConnection.setRequestMethod("POST");
+				urlConnection.setDoInput(true);
 
-				strResult = EntityUtils.toString(response.getEntity(), HTTP.UTF_8);
-				System.out.println("strResult = " + strResult);
+				// Add any data you wish to post here
+				urlConnection.connect();
+				InputStream in = urlConnection.getInputStream();
 
+				if(in != null) {
+					BufferedReader br = new BufferedReader(new InputStreamReader(in));
+					String inputLine;
+
+					while ((inputLine = br.readLine()) != null) {
+						System.out.println("MainActivity / inputLine = " + inputLine);
+						strResult += inputLine;
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			// HTTP POST
+//	        String url = "http://10.1.1.3:8080/LiteNoteWeb/viewNote/viewNote_json.jsp";
+//			try {
+//				HttpClient client = new DefaultHttpClient();
+//				HttpPost post = new HttpPost(url);
+//
+//				HttpResponse response = client.execute(post);
+//
+//				strResult = EntityUtils.toString(response.getEntity(), HTTP.UTF_8);
+//				System.out.println("strResult = " + strResult);
+//
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+
+			System.out.println("MainActivity / result final = " + strResult);
+
+
+			// JSON array
+			try {
 				JSONArray jsonArray = new JSONArray(strResult);
 
 				//Set Array size
@@ -89,27 +146,73 @@ public class MainActivity extends AppCompatActivity {
 				Uri = new String[jsonArray.length()];
 				Title = new String[jsonArray.length()];
 
-
-				for(int i=0;i<jsonArray.length();i++) {
-					JSONObject jsonObject = (JSONObject)jsonArray.get(i);
+				for (int i = 0; i < jsonArray.length(); i++) {
+					JSONObject jsonObject = (JSONObject) jsonArray.get(i);
 					Id[i] = jsonObject.getInt("note_id");
 					Uri[i] = jsonObject.getString("note_link_uri");
 					Title[i] = jsonObject.getString("note_title");
 				}
-
-
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				e.printStackTrace();
 			}
+
 			return null;
 		}
 		@Override
 		protected void onPostExecute(Void Result){
 			MyAdapter adapter = new MyAdapter(MainActivity.this);
 			list.setAdapter(adapter);
+
+			list.setItemsCanFocus(true);
+			list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+					String idStr = getYoutubeId(Uri[position]);
+					Intent intent = YouTubeIntents.createPlayVideoIntentWithOptions(MainActivity.this, idStr, false/*fullscreen*/, true/*finishOnEnd*/);
+					startActivity(intent);
+				}
+			});
+
 			super.onPostExecute(Result);
 		}
 	}
+
+	private void trustEveryone() {
+		try {
+			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}});
+
+			SSLContext context = SSLContext.getInstance("TLS");
+
+			context.init(null, new X509TrustManager[]
+							{
+								new X509TrustManager()
+								{
+									public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException
+									{}
+
+									public void checkServerTrusted(X509Certificate[] chain,String authType) throws CertificateException
+									{}
+
+									public X509Certificate[] getAcceptedIssuers() {
+										return new X509Certificate[0];
+									}
+								}
+							},
+					new SecureRandom()
+			);
+
+			HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+
+		} catch (Exception e) { // should never happen
+			e.printStackTrace();
+		}
+	}
+
 
 	public class MyAdapter extends BaseAdapter {
 		private LayoutInflater inflater;
@@ -146,7 +249,7 @@ public class MainActivity extends AppCompatActivity {
 			String pictureUri = "http://img.youtube.com/vi/"+getYoutubeId(Uri[i])+"/0.jpg";
 			//System.out.println("pictureUri = " + pictureUri);
 
-			aName.setOnClickListener(new View.OnClickListener() {
+			view.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					String idStr = getYoutubeId(Uri[i]);
@@ -154,6 +257,10 @@ public class MainActivity extends AppCompatActivity {
 					startActivity(intent);
 				}
 			});
+
+			view.setFocusable(true);
+			view.setClickable(true);
+
 
 			aName.setText(String.valueOf(Id[i]));
 			aAddress.setText(Uri[i]);
